02a79d19298096dcc10daa7f155fba708106bed8
Lan <lanhuidong@gmail.com>
2016-11-16 10:20:38 +0800

    新增车辆管理API
    添加车牌、删除车牌

---
+++ src/main/java/com/zeno/intelvision/request/VehicleAddPlateRequest.java
@@ -0,0 +1,38 @@
+package com.zeno.intelvision.request;
+
+import com.zeno.intelvision.annotation.Param;
+import com.zeno.intelvision.response.VehicleAddPlateResponse;
+
+/**
+ * @author lanhuidong
+ * @since 2016-11-15
+ */
+public class VehicleAddPlateRequest extends ZenoRequest<VehicleAddPlateResponse> {
+
+    @Param(name = "vehicle_id")
+    private String vehicleId;
+
+    @Param(name = "plate_id")
+    private String plateId;
+
+    public String getVehicleId() {
+        return vehicleId;
+    }
+
+    public void setVehicleId(String vehicleId) {
+        this.vehicleId = vehicleId;
+    }
+
+    public String getPlateId() {
+        return plateId;
+    }
+
+    public void setPlateId(String plateId) {
+        this.plateId = plateId;
+    }
+
+    @Override
+    public String getApi() {
+        return "/vehicle/add_plate";
+    }
+}
---
+++ src/main/java/com/zeno/intelvision/response/VehicleAddPlateResponse.java
@@ -0,0 +1,27 @@
+package com.zeno.intelvision.response;
+
+/**
+ * @author lanhuidong
+ * @since 2016-11-15
+ */
+public class VehicleAddPlateResponse extends ZenoResponse {
+
+    private boolean success;
+    private int added;
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public int getAdded() {
+        return added;
+    }
+
+    public void setAdded(int added) {
+        this.added = added;
+    }
+}
